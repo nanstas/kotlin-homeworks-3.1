2693e920ef97569927989cc005ed096ed7932440
Stanislav Nanoshkin <nanstas@yandex.ru>
2020-09-09 21:47:40 +0500

    fix tests

--- src/test/kotlin/ru/netology/WallServiceTest.kt
+++ src/test/kotlin/ru/netology/WallServiceTest.kt
@@ -225,6 +225,6 @@ class WallServiceTest {
         val result = service.update(update)
 
         // проверяем результат (используйте assertTrue или assertFalse)
-        assertTrue(result)
+        assertFalse(result)
     }
 }
